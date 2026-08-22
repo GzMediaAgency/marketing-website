67e751719151416ea0e513cd163a899427f83148
GzMediaAgency <gz.app.media@gmail.com>
2026-08-23 00:28:15 +0100

    Create Providers.tsx

---
+++ components/layout/Providers.tsx
@@ -0,0 +1,6 @@
+'use client';
+import { LanguageProvider } from './LanguageContext';
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return <LanguageProvider>{children}</LanguageProvider>;
+}
