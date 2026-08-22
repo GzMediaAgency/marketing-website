aeb22b6d1b604016fdd504b2e41729ed44dcc151
GzMediaAgency <gz.app.media@gmail.com>
2026-08-22 23:29:31 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
   },
+  i18n: {
+    locales: ['en', 'fr'],
+    defaultLocale: 'en',
+    localeDetection: true,
+  },
 };
 
 module.exports = nextConfig;
